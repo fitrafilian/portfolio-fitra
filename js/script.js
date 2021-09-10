e84aa081e281aea8d129e417caebefb1c6d5cad4
fitrafilian <filian.fitra@gmail.com>
2021-09-10 14:42:11 +0700

    update navigation bar

--- js/script.js
+++ js/script.js
@@ -9,8 +9,6 @@ let observer = new IntersectionObserver(navCheck, options);
 function navCheck(entries) {
   entries.forEach((entry) => {
     const id = entry.target.id;
-    // const activeAnchor = document.querySelector(`[data-page=${id}]`);
-    // const coord = activeAnchor.getBoundingClientRect();
     let page = document.querySelector(`[data-page=${id}]`);
     page.classList.remove("active");
 
